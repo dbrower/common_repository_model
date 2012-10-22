194c1619455ca8a74f6f4ddf95fc14bf62eb76da
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2012-10-22 10:54:22 -0400

    Adding PersistenceBase for initial work

---
+++ lib/common_repository_model/persistence_base.rb
@@ -0,0 +1,5 @@
+require 'active_fedora'
+module CommonRepositoryModel
+  class PersistenceBase < ActiveFedora::Base
+  end
+end
---
+++ spec/common_respository_model/persistence_base_spec.rb
@@ -0,0 +1,6 @@
+lib = File.expand_path('../../../lib', __FILE__)
+require 'minitest/autorun'
+require File.join(lib,'common_repository_model/persistence_base')
+
+describe CommonRepositoryModel::PersistenceBase do
+end
