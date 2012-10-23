5cbd401c8099553b825d8eaf16964041b39875a7
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2012-10-23 11:58:11 -0400

    Adding Collection.members

--- spec/common_respository_model/collection_spec.rb
+++ spec/common_respository_model/collection_spec.rb
@@ -34,4 +34,8 @@ describe CommonRepositoryModel::Collection do
     assert_respond_to subject, :area_id=
   end
 
+  it 'should have_many #members' do
+    subject.members.must_be_kind_of(Array)
+  end
+
 end
