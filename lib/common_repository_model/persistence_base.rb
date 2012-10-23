30c3d7e6ebd677670d5eb5f4b1c71f7d95585f80
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2012-10-23 09:43:36 -0400

    Adding foundational persistence model

--- lib/common_repository_model/persistence_base.rb
+++ lib/common_repository_model/persistence_base.rb
@@ -1,5 +1,6 @@
 require 'active_fedora'
 module CommonRepositoryModel
   class PersistenceBase < ActiveFedora::Base
+    include ActiveFedora::Relationships
   end
 end
--- spec/common_respository_model/persistence_base_spec.rb
+++ spec/common_respository_model/persistence_base_spec.rb
@@ -3,4 +3,11 @@ require 'minitest/autorun'
 require File.join(lib,'common_repository_model/persistence_base')
 
 describe CommonRepositoryModel::PersistenceBase do
+
+  subject { CommonRepositoryModel::PersistenceBase.new }
+
+  it 'should include ActiveFedora::Relationships' do
+    subject.class.included_modules.must_include(ActiveFedora::Relationships)
+  end
+
 end
