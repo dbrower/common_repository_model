6482085df4e4e6272822b2e3532c8cbfd4bc5ebd
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2012-10-24 10:09:30 -0400

    Adding ActiveFedora config for testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,8 @@
 $:.unshift File.expand_path('../../lib', __FILE__)
 require 'minitest/autorun'
+require 'active_fedora'
+ActiveFedora.init(
+  fedora_config_path: File.join(File.dirname(__FILE__),"config/fedora.yml"),
+  solr_config_path: File.join(File.dirname(__FILE__),"config/solr.yml"),
+  environment: :test
+)
