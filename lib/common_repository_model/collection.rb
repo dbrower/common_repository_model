f68f2910f791c3302816d685d98ffcaa31158ca2
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2012-10-24 15:33:11 -0400

    Working on Collection membership maintenance
    Presently the specs are working. Need to setup such that both sides
    don't store relationships @WIP.

--- lib/common_repository_model/collection.rb
+++ lib/common_repository_model/collection.rb
@@ -1,8 +1,51 @@
 require_relative './persistence_base'
 require_relative './area'
+require 'set'
 
 module CommonRepositoryModel
+  class MembershipRegistry
+    def initialize
+      @has_members = Set.new
+      @is_member_of = Set.new
+    end
+    attr_reader :has_members, :is_member_of
+  end
   class Collection < PersistenceBase
+
+    def self.membership_registry
+      @membership_registry ||= MembershipRegistry.new
+    end
+
+    def membership_registry
+      self.class.membership_registry
+    end
+
+    def self.has_members(method_name, *args)
+      membership_registry.has_members << method_name
+      has_and_belongs_to_many(method_name, *args)
+    end
+
+    def self.is_member_of(method_name, *args)
+      membership_registry.is_member_of << method_name
+      has_and_belongs_to_many(method_name, *args)
+    end
+
+    def save
+      collected_has_members = membership_registry.has_members.
+      collect do |association_name|
+        public_send(association_name)
+      end.flatten
+      self.child_collections = collected_has_members
+
+      collected_is_member_of = membership_registry.is_member_of.
+      collect do |association_name|
+        public_send(association_name)
+      end.flatten
+
+      self.contained_in_collections = collected_is_member_of
+
+      super
+    end
 
     belongs_to(
       :area,
@@ -14,13 +57,13 @@ module CommonRepositoryModel
       :child_collections,
       class_name: 'CommonRepositoryModel::Collection',
       property: :is_member_of,
-      inverse_of: :has_membership_in
+      inverse_of: :has_members
     )
 
     has_and_belongs_to_many(
-      :parent_collections,
+      :contained_in_collections,
       class_name: 'CommonRepositoryModel::Collection',
-      property: :has_membership_in,
+      property: :has_members,
       inverse_of: :is_member_of
     )
 
--- spec/common_respository_model/integration_spec.rb
+++ spec/common_respository_model/integration_spec.rb
@@ -1,31 +1,73 @@
 require_relative '../spec_helper'
 require 'common_repository_model/collection'
 
-class Person < CommonRepositoryModel::Collection
+class Job < CommonRepositoryModel::Collection
   has_and_belongs_to_many(
+    :people,
+    class_name: 'Person',
+    property: :has_workers,
+    inverse_of: :is_working_as
+  )
+end
+
+class Family < CommonRepositoryModel::Collection
+  has_members(
+    :family_members,
+    class_name: 'Person',
+    property: :has_family_members,
+    inverse_of: :is_family_member_of
+  )
+end
+
+class Person < CommonRepositoryModel::Collection
+
+  is_member_of(
     :parents,
     class_name: 'Person',
     property: :is_child_of,
     inverse_of: :is_parent_of
   )
 
-  has_and_belongs_to_many(
+  has_members(
     :children,
     class_name: 'Person',
     property: :is_parent_of,
     inverse_of: :is_child_of
   )
 
+  # People can be part of multiple families
+  is_member_of(
+    :families,
+    class_name: 'Family',
+    property: :is_family_member_of,
+    inverse_of: :has_family_members,
+  )
+
+  has_and_belongs_to_many(
+    :jobs,
+    class_name: 'Job',
+    property: :is_working_as,
+    inverse_of: :has_workers
+  )
+
 end
 
 describe CommonRepositoryModel::Collection do
+  let(:family) { Family.new }
+  let(:lawyer) { Job.new }
+  let(:doctor) { Job.new }
   let(:heathcliff) { Person.new }
   let(:claire) { Person.new }
   let(:theo) { Person.new }
   let(:vanessa) { Person.new }
   let(:rudy) { Person.new }
 
-  it {
+  it 'verifies complicated relationships' do
+    family.save
+
+    lawyer.save
+    doctor.save
+
     heathcliff.save
     claire.save
     theo.save
@@ -33,16 +75,43 @@ describe CommonRepositoryModel::Collection do
     rudy.save
 
     claire.children = [theo,vanessa,rudy]
+    claire.jobs << lawyer
+    claire.families << family
     claire.save
+
     heathcliff.children = [theo,vanessa,rudy]
+    heathcliff.jobs << doctor
+    heathcliff.families << family
     heathcliff.save
 
+    theo.families << family
+    theo.save
+    vanessa.families << family
+    vanessa.save
+    rudy.families << family
+    rudy.save
+
     reloaded_theo = theo.class.find(theo.pid)
-    reloaded_theo.parent_collections.size.must_equal 2
+
+    reloaded_theo.jobs.size.must_equal 0
+    reloaded_theo.families.size.must_equal 1
     reloaded_theo.parents.size.must_equal 2
 
+    reloaded_theo.child_collections.count.must_equal 0
+
+    reloaded_theo.contained_in_collections.must_include(heathcliff)
+    reloaded_theo.contained_in_collections.must_include(claire)
+    reloaded_theo.contained_in_collections.must_include(family)
+
     reloaded_claire = claire.class.find(claire.pid)
-    reloaded_claire.child_collections.size.must_equal 3
+    reloaded_claire.jobs.size.must_equal 1
+    reloaded_claire.families.size.must_equal 1
     reloaded_claire.children.size.must_equal 3
-  }
+
+    reloaded_claire.child_collections.must_include(theo)
+    reloaded_claire.child_collections.must_include(vanessa)
+    reloaded_claire.child_collections.must_include(rudy)
+
+    reloaded_claire.contained_in_collections.must_include(family)
+  end
 end
