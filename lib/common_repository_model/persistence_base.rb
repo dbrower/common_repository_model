86acd7b5bd9cb5920ed9c3a22dd1ee00e529d312
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2012-10-31 09:06:50 -0400

    Removing ActiveFedora::Relationships module

--- lib/common_repository_model/persistence_base.rb
+++ lib/common_repository_model/persistence_base.rb
@@ -1,6 +1,5 @@
 require 'active_fedora'
 module CommonRepositoryModel
   class PersistenceBase < ActiveFedora::Base
-    include ActiveFedora::Relationships
   end
 end
--- spec/common_respository_model/persistence_base_spec.rb
+++ spec/common_respository_model/persistence_base_spec.rb
@@ -5,8 +5,4 @@ describe CommonRepositoryModel::PersistenceBase do
 
   subject { CommonRepositoryModel::PersistenceBase.new }
 
-  it 'should include ActiveFedora::Relationships' do
-    subject.class.included_modules.must_include(ActiveFedora::Relationships)
-  end
-
 end
