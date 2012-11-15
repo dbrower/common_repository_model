cdc26b78bccef7c03f3ff3bf80335a514d1b21ba
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2012-11-15 09:55:49 -0500

    Switching Area spec to use FactoryGirl

--- spec/common_respository_model/area_spec.rb
+++ spec/common_respository_model/area_spec.rb
@@ -3,40 +3,44 @@ require 'common_repository_model/area'
 require 'common_repository_model/collection'
 
 describe CommonRepositoryModel::Area do
-
-  subject { CommonRepositoryModel::Area.new(name: name) }
-  let(:name) { 'My Area Name'}
-
-  it 'should find or create by #name' do
-    area = CommonRepositoryModel::Area.new(name: name)
-    lambda { area.save }.must_raise(NoMethodError)
-    lambda { area.save! }.must_raise(NoMethodError)
+  describe 'meta test' do
+    it {
+      FactoryGirl.create(:area).persisted?.must_equal true
+    }
   end
 
-  it 'should require a #name' do
-    subject.name = nil
-    subject.valid?.must_equal false
-    subject.name = name
-    subject.valid?.must_equal true
-  end
+  describe 'without persisting' do
+    subject { FactoryGirl.build(:area, name: name) }
+    let(:name) { 'My Area Name'}
 
-  it 'should have #name' do
-    subject.name.must_equal name
-  end
+    it 'should find or create by #name' do
+      lambda { subject.save }.must_raise(NoMethodError)
+      lambda { subject.save! }.must_raise(NoMethodError)
+    end
 
-  describe 'has_many #collections' do
+    it 'should require a #name' do
+      subject.name = nil
+      subject.valid?.must_equal false
+      subject.name = name
+      subject.valid?.must_equal true
+    end
+
+    it 'should have #name' do
+      subject.name.must_equal name
+    end
+
     it 'should build #collections' do
       subject.collections.build.
         must_be_kind_of(CommonRepositoryModel::Collection)
     end
   end
 
-  describe 'integration' do
-    let(:collection) { CommonRepositoryModel::Collection.new }
+  describe 'integration (with persistence)' do
+    subject { FactoryGirl.create(:area) }
+    let(:collection) { FactoryGirl.build(:collection) }
     it 'should save' do
       # Before we can add a collection, the containing object
       # must be saved
-      subject.send(:save).must_equal true
       subject.collections << collection
       subject.send(:save).must_equal true
       new_subject = subject.class.find(subject.pid)
