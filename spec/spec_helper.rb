0730f02fe230aaabd1e3e03d7e2718f6d1cc622a
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2012-11-15 09:55:16 -0500

    Adding FactoryGirl to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 $:.unshift File.expand_path('../../lib', __FILE__)
 require 'minitest/autorun'
 require 'active_fedora'
+require 'factory_girl'
+FactoryGirl.find_definitions
 ActiveFedora.init(
   fedora_config_path: File.join(File.dirname(__FILE__),"config/fedora.yml"),
   solr_config_path: File.join(File.dirname(__FILE__),"config/solr.yml"),
