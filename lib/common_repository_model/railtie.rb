ece8ecffeb306c2c20e563443ecf9497ba5271ed
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2012-11-20 15:32:57 -0500

    Updating collection generator to include factory

--- lib/common_repository_model/railtie.rb
+++ lib/common_repository_model/railtie.rb
@@ -3,5 +3,10 @@ module CommonRepositoryModel
     rake_tasks do
       load "tasks/common_repository_model.tasks"
     end
+    generators do
+      require(
+        'generators/common_repository_model/collection/collection_generator'
+      )
+    end
   end
 end
--- lib/generators/common_repository_model/collection/collection_generator.rb
+++ lib/generators/common_repository_model/collection/collection_generator.rb
@@ -1,6 +1,9 @@
 require 'rails/generators'
-class CommonRepositoryModel::Collection < Rails::Generators::NamedBase
+
+class CommonRepositoryModel::CollectionGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../templates', __FILE__)
+  argument :attributes, :type => :array, :default => [], :banner => "field:type field:type"
+  class_option :test_dir, :type => :string, :default => "spec/factories", :desc => "The directory where the factories should go"
 
   def create_collection
     template(
@@ -8,10 +11,16 @@ class CommonRepositoryModel::Collection < Rails::Generators::NamedBase
       File.join('app/repository_models/', "#{file_name}.rb")
     )
   end
+
   def create_service_spec
     template(
       'collection_spec.rb.erb',
       File.join('spec/repository_models/', "#{file_name}_spec.rb")
     )
   end
+
+  def create_fixture_file
+    template 'collection_factory.rb.erb', File.join(options[:test_dir], "#{file_name}_factory.rb")
+  end
+
 end
