6b961870199d8380ffe08afdd5e8fd2be66eb7d8
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2012-11-28 12:50:17 -0500

    Moving custom assertions to TestSupport module

--- lib/common_repository_model/test_support.rb
+++ lib/common_repository_model/test_support.rb
@@ -1,5 +1,26 @@
 module CommonRepositoryModel
   module TestSupport
+    def assert_rels_ext(subject, predicate, objects = [])
+      assert_equal objects.count, subject.relationships(predicate).count
+      objects.each do |object|
+        internal_uri = object.respond_to?(:internal_uri) ?
+          object.internal_uri : object
+        assert subject.relationships(predicate).include?(internal_uri)
+      end
+    end
+
+    def assert_active_fedora_belongs_to(subject, method_name, object)
+      subject.send(method_name).must_equal object
+    end
+
+    def assert_active_fedora_has_many(subject, method_name, objects)
+      association = subject.send(method_name)
+      assert_equal objects.count, association.count
+      objects.each do |object|
+        assert association.include?(object)
+      end
+    end
+
     def with_persisted_area(name = nil)
       options = {}
       options[:name] = name if name
