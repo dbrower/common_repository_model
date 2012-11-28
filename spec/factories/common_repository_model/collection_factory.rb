dd7a8a48b48015ebf13cf029ab3b504fc4016e1e
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2012-11-28 16:18:09 -0500

    Updating generators to more readily handle area
    At some point, I don't want to be hitting Fedora to generate all of
    these objects. By leaning on Factories, this will be easier in the
    future.

--- spec/factories/common_repository_model/collection_factory.rb
+++ spec/factories/common_repository_model/collection_factory.rb
@@ -2,5 +2,10 @@ require_relative '../../../lib/common_repository_model/data'
 
 FactoryGirl.define do
   factory :common_repository_model_collection, class: CommonRepositoryModel::Collection do
+    before(:create) { |collection|
+      area_name = collection.name_of_area_to_assign
+      CommonRepositoryModel::Area.find_by_name(area_name) ||
+      FactoryGirl.create(:common_repository_model_area, name: area_name)
+    }
   end
 end
