cf135b308c1e02a2d4bf08c08e8b4f79620fc3d9
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-01-10 11:17:37 -0500

    Updating Collection generator to include metadata

--- lib/generators/common_repository_model/collection/collection_generator.rb
+++ lib/generators/common_repository_model/collection/collection_generator.rb
@@ -1,7 +1,7 @@
 require 'rails/generators'
 
 class CommonRepositoryModel::CollectionGenerator < Rails::Generators::NamedBase
-  check_class_collision
+
   source_root File.expand_path('../templates', __FILE__)
   class_option :test_dir, :type => :string, :default => "spec/factories", :desc => "The directory where the factories should go"
   argument :attributes, :type => :array, :default => [], :banner => "field:type field:type"
@@ -13,10 +13,24 @@ class CommonRepositoryModel::CollectionGenerator < Rails::Generators::NamedBase
     )
   end
 
-  def create_service_spec
+  def create_collection_spec
     template(
       'collection_spec.rb.erb',
       File.join('spec/repository_models/', "#{file_name}_spec.rb")
+    )
+  end
+
+  def create_metadata_datastream
+    template(
+      'metadata_datastream.rb.erb',
+      File.join('app/repository_datastreams/', "#{file_name}_metadata_datastream.rb")
+    )
+  end
+
+  def create_metadata_datastream_spec
+    template(
+      'metadata_datastream_spec.rb.erb',
+      File.join('spec/repository_datastreams/', "#{file_name}_metadata_datastream_spec.rb")
     )
   end
 
